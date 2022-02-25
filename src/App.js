77ddd0d58f5b86e3ca3355be7e061a7cfbf2e611
AdrianSadowski <sadow.adrian@gmail.com>
2022-02-25 21:24:25 +0100

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './styles/bootstrap.scss';
+import './styles/global.scss';
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,28 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState, useEffect} from 'react';
+import logo from '../../../images/logo.png';
+import {Link} from 'react-router-dom';
 
 import styles from './Header.module.scss';
 
-const Component = ({children}) => (
-  <div className={ styles.root}>
-    <h2>Header</h2>
-    {children}
-  </div>
-);
+const Component = () => {
+  const [toogleMenu, setToogleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-Component.propTypes = {
-  children: PropTypes.node,
+  const toogleNav = () => {
+    setToogleMenu(!toogleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+      window.removeEventListener('resize', changeWidth);
+    };
+  }, []);
+  return (
+    <div className="row">
+      <div className="col-lg-7 row">
+        <div className={'col-6 ' + styles.logo}>
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <nav className={'col-6 ' + styles.root}>
+          {(toogleMenu || screenWidth > 576) && (
+            <ul className={styles.list}>
+              <Link to="/onas">
+                <li className={styles.items}>o nas</li>
+              </Link>
+              <Link to="/oferta">
+                <li className={styles.items}>oferta</li>
+              </Link>
+              <Link to="/kontakt">
+                <li className={styles.items}>kontakt</li>
+              </Link>
+            </ul>
+          )}
+          <button onClick={toogleNav} className={styles.btn}>
+            BTN
+          </button>
+        </nav>
+      </div>
+      <div className={'col-lg-5 ' + styles.rightnav}></div>
+    </div>
+  );
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+Component.propTypes = {};
 
 export {
   Component as Header,
